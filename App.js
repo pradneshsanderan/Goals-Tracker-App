95ea1d4fea5e7001a4ada923386ab8e6bac75294
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-06-02 19:41:13 +0000

    create app and add search

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{padding:50}} >
+      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+        <TextInput placeholder="Add Goals"
+        style={{width:'80%', borderBottomWidth:1, padding:10, borderBottomColor:'black'}}/>
+        <Button title="ADD"/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
