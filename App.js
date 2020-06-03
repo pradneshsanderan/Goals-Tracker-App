ff50272f27c5e3809b8b11aa54d19f9d9f3a1241
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-06-03 19:31:58 +0000

    new commit

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal ] = useState('');
+
+  const goalInputHandler= (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () =>{
+    console.log(enteredGoal);
+  };
+
   return (
-    <View style={{padding:50}} >
-      <View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+    <View style={styles.screen} >
+      <View style={styles.inputContainer}>
         <TextInput placeholder="Add Goals"
-        style={{width:'80%', borderBottomWidth:1, padding:10, borderBottomColor:'black'}}/>
-        <Button title="ADD"/>
+          style={styles.input} onChangeText={goalInputHandler}
+          value={enteredGoal} />
+        <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
 
@@ -17,5 +28,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%', 
+    borderBottomWidth: 1, 
+    padding: 10, 
+    borderBottomColor: 'black'
+  }
 });
